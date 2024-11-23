1865b91c39f471f50b769d26fa95e21c0731dd3c
Little clouds <36187672+littleclouds@users.noreply.github.com>
2024-11-23 21:35:10 +0530

    Create Longest Palindrome.java

---
+++ Longest Palindrome.java
@@ -0,0 +1,35 @@
+/*Given a string s, your task is to find the longest palindromic substring within s. 
+A substring is a contiguous sequence of characters within a string, defined as s[i...j] where 0 ≤ i ≤ j < len(s).
+A palindrome is a string that reads the same forward and backward. More formally, s is a palindrome if reverse(s) == s.
+The string s consists of only lowercase English letters ('a' to 'z'). */
+
+public class LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+    public static void main(String[] args) {
+        LongestPalindromicSubstring solution = new LongestPalindromicSubstring();
+        String s = "babad";
+        System.out.println("Longest Palindromic Substring of " + s + " is: " + solution.longestPalindrome(s));
+    }
+}
